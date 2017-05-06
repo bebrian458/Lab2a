3ad7385ac8cea409b72c441fe0ad9e78382427c6
Brian Be <bebrian458@gmail.com>
2017-05-05 18:40:42 -0700

    Got basic add-none working

--- lab2_add.c
+++ lab2_add.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <getopt.h>
+#include <getopt.h>	
 #include <pthread.h>
+#include <time.h>
 
 // Global variables
-int numthreads, numIters;
-long long counter;
+int numthreads = 1, numIters = 1;
 
 
 // Basic add routine
@@ -14,16 +14,39 @@ void add(long long *pointer, long long value){
         *pointer = sum;
 }
 
-// // Thread routine
-// void* worker(){
-//
-// }
+// Thread routine
+void* worker(void* counter){
+
+	// Add by 1
+	int i;
+	for(i = 0; i < numIters; i++){
+
+		// Without locks
+		add((long long *) counter, 1);
+//			fprintf(stderr, "%lld\n", *((long long*)counter));
+	}
+
+	// Add by -1
+	int j;
+	for(j = 0; j < numIters; j++){
+
+		// Without locks
+		add((long long *) counter, -1);
+//		fprintf(stderr, "%lld\n", *((long long*)counter));
+
+	}
+	return NULL;
+}	
 
 // Main routine 
 int main(int argc, char *argv[]){
 
 	// Default values
-	int opt = 0, numthreads = 1, numIters = 1, counter = 0;
+	int opt = 0;
+	long long counter = 0;
+
+	// Timer structs
+	struct timespec start, end;
 
 	struct option longopts[] = {
 		{"threads", 	required_argument, 	NULL, 't'},
@@ -46,12 +69,52 @@ int main(int argc, char *argv[]){
 		}
 	}
 
+
+	// Start timer
+	if(clock_gettime(CLOCK_MONOTONIC, &start) == -1){
+		fprintf(stderr, "Error starting timer\n");
+		exit(1);
+	}
+
+	// Allocate memory for threads
+	pthread_t *threads = malloc(numthreads*sizeof(pthread_t));
+	if(threads == NULL){
+		fprintf(stderr, "Error allocating memory for threads\n");
+		exit(1);
+	}
+
 	// Create threads
 	int i;
 	for(i = 0; i < numthreads; i++){
-
+		if(pthread_create(threads+i, NULL, worker, &counter) != 0){
+			fprintf(stderr, "Error creating threads\n");
+			exit(1);
+		}
 	}
 
 	// Join threads
+	for(i = 0; i < numthreads; i++){
+		if(pthread_join(*(threads+i), NULL) != 0){
+			fprintf(stderr, "Error creating threads\n");
+			exit(1);
+		}
+	}
 
+	// Stop timer
+	if(clock_gettime(CLOCK_MONOTONIC, &end) == -1){
+		fprintf(stderr, "Error stopping timer\n");
+		exit(1);
+	}
+
+	// Calculations
+	int numops = numthreads * numIters * 2;
+	long long total_time = 1000000000 * (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec);
+	long long avg_time_per_op = total_time/numops;
+
+
+	// Print CSV
+	fprintf(stdout, "add-none,%d,%d,%d,%lld,%lld,%lld\n",
+		numthreads,numIters,numops,total_time,avg_time_per_op,counter);
+
+	return 0;
 }
